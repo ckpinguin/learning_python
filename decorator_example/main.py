b3803b5929a38270f492d81f7254aaee4b8e0986
Christof Kälin <christof.kaelin@pm.me>
2024-01-27 16:30:17 +0100

    more advanced examples (wrapper with args and decorator with args)

--- decorator_example/main.py
+++ decorator_example/main.py
@@ -18,7 +18,29 @@ def delay_decorator_with_args(sleep_seconds):
     return delay_decorator
 
 
+current_time = time.time()
+
+
+def timer_decorator(function):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        function(*args, **kwargs)
+        diff_time = time.time() - start_time
+        print(f"{function.__name__} ran {diff_time} seconds.")
+    return wrapper
+
+
+@timer_decorator
+def long_running(number):
+    for i in range(0, number):
+        n = i
+
+
+long_running(200000000)
+
+
 @delay_decorator
+@timer_decorator
 def say_hello():
     print("Hello")
 
@@ -31,5 +53,6 @@ def say(text):
 decorated_say_hello = delay_decorator(say_hello)
 
 say("huhuu")
+say(text="yup")
 say_hello()
 decorated_say_hello()
