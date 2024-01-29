2782a50a994a4097a6e6462e05dd89f0d25cc2a4
Christof Kälin <christof.kaelin@pm.me>
2024-01-29 16:36:09 +0100

    some more decorator experiments with flask

--- flask_first_try/main.py
+++ flask_first_try/main.py
@@ -8,5 +8,43 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
+def make_bold_with_args(function):
+    def wrapper(*args, **kwargs):
+        function(*args, **kwargs)
+    return wrapper
+
+
+@app.route("/bye")
+@make_bold
+@make_underlined
+@make_emphasis
+def bye():
+    return "Bye!"
+
+
+@app.route("/<name>/<int:number>")
+@make_bold_with_args
+def greet(name, number):
+    return f"Hello there, {name}, {number}!"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
