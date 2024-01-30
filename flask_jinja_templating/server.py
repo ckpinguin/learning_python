2af1cd19bf992612414002e65341267e9ec2827c
Christof Kälin <christof.kaelin@pm.me>
2024-01-30 16:30:32 +0100

    more flask routing expeiments

--- flask_jinja_templating/server.py
+++ flask_jinja_templating/server.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import random
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello world"
+    rand_num = random.randint(1, 10)
+    return render_template('index.html', num=rand_num, year=2024, name="Jim")
+
+
+@app.route('/guess/<name>')
+def guess_name_age(name):
+    response = requests.get(f'https://api.agify.io?name={name}')
+    data = response.json()
+    age = data["age"]
+    return f"<p>You might be {age} years old."
 
 
 if __name__ == "__main__":
