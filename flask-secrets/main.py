512fda0d8ad3cfd9487d5e0dbff898f7329d6353
Christof Kälin <christof.kaelin@pm.me>
2024-02-02 16:11:20 +0100

    using flask / jinja / wtf forms

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 '''
 Red underlines? Install the required packages first: 
@@ -15,11 +19,33 @@ This will install the packages from requirements.txt for this project.
 
 
 app = Flask(__name__)
+app.secret_key = "some secret string"
+bootstrap = Bootstrap5(app)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(label='email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='password', validators=[
+                             DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        email = login_form.email.data
+        password = login_form.password.data
+        if email == 'yourmail@yahoo.com' and password == '12345':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
