87baf0919952e89900ab09a9a782f980815b9349
Christof Kälin <christof.kaelin@pm.me>
2024-02-13 11:37:15 +0100

    finished day 68

--- day-68-flask-auth/main.py
+++ day-68-flask-auth/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import (Flask, render_template, request, url_for,
+                   redirect, flash, send_from_directory)
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -20,11 +21,16 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# CREATE TABLE IN DB
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -37,32 +43,55 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",
+                           logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter(User.email == email).scalar()
+        if user:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for("login"))
         hash_salt_pw = generate_password_hash(
-            request.form.get("password"),
-            method='pbkdf2:sha256', salt_length=8)
-        new_user = User(request.form.get('email'),
-                        name=request.form.get('name'),
+            password, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(email=email,
+                        name=name,
                         password=hash_salt_pw)
         db.session.add(new_user)
         db.session.commit()
-        return render_template("secrets.html", name=request.form.get("name"))
-    return render_template("register.html")
+        return render_template("secrets.html")
+    return render_template("register.html",
+                           logged_in=current_user.is_authenticated)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter(User.email == email).scalar()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
+        else:
+            flash("Wrong credentials!", "error")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",
+                           logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html",
+                           name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
@@ -72,8 +101,9 @@ def logout():
 
 
 @app.route('/download')
+@login_required
 def download():
-    return send_from_directory('static', filename="files/cheat_sheet.pdf")
+    return send_from_directory('static', path="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
