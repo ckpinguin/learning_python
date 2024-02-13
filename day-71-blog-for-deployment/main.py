f19970c8a3126fb83765badea6b047f451982e52
Christof Kälin <christof.kaelin@pm.me>
2024-02-13 21:11:30 +0100

    Remove unnecessary comments and import statements

--- day-71-blog-for-deployment/main.py
+++ day-71-blog-for-deployment/main.py
@@ -14,21 +14,6 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # import smtplib
 import os
 
-
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 # '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
