666f5f4bf5ef706a108888c5177f2f8d51527183
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-15 15:42:34 +0200

    day 94 dinosaur game, solution grabbed, not working on my screen (dual, hidpi) coordinades though

--- 94-dinosaur-game-bot/main.py
+++ 94-dinosaur-game-bot/main.py
@@ -1,7 +1,35 @@
+import time
+import pyautogui
+from PIL import ImageGrab, ImageOps
+import numpy as np
 
 
-GAME_URL = 'https://elgoog.im/dinosaur-game/'
+# Solution copied from Prasad Khuntias code, with minor modifications
+
+# Region where the dino jumps (left, top, right, bottom)
+dino_coords = (696, 420, 3800, 500)
+# Region where obstacles appear (left, top, right, bottom)
+obstacle_coords = (881, 420, 3600, 500)
 
 
-if __name__ == '__main__':
-    print('Hello, World!')
+def check_obstacle():
+    image = ImageGrab.grab(bbox=obstacle_coords)
+    gray_image = ImageOps.grayscale(image)
+    a = np.array(gray_image.getcolors())
+    return a.sum() < 1000
+
+
+def jump():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    pyautogui.keyUp('space')
+
+
+print("Starting game in 3 seconds...")
+time.sleep(3)
+
+while True:
+    if check_obstacle():
+        jump()
+        time.sleep(0.1)
+    time.sleep(0.1)
